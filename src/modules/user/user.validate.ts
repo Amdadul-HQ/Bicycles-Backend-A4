f125cabdb0f6f369c2fe401acbed4b32eeff7b31
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-28 16:46:16 +0600

    add: sign up function successfull

--- src/modules/user/user.validate.ts
+++ src/modules/user/user.validate.ts
@@ -6,16 +6,14 @@ const createUserValidationSchema = z.object({
   body: z.object({
     name: z
       .string()
-      .min(1, 'Name is Required')
-      .max(20, 'Name Can Not Be More Than 20 Characters'),
+      .min(1, 'Name is Required'),
     email: z
       .string()
       .email('Invalid Email Address')
       .min(1, 'User Email is Required'),
     password: z
       .string()
-      .min(1, 'Password is Required')
-      .max(16, 'Password can not be more then 20 Characters'),
+      .min(1, 'Password is Required'),
     role: z.enum([...Role] as [string, ...string[]]).default('customer').optional(),
     isBlocked:z.boolean().default(false).optional()
   }),
