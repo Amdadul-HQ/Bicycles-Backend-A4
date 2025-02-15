1a2bdc4e4aac13c6915f0f4b5c9bcc0f39a27ae8
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-15 16:37:28 +0600

    remove:console log

--- src/modules/product/product.route.ts
+++ src/modules/product/product.route.ts
@@ -14,7 +14,6 @@ router.post('/add-bicycle',
     upload.single('file'),
     (req:Request,res:Response,next:NextFunction)=>{
     req.body = JSON.parse(req.body.data);
-    console.log(req.body.data,'hello from ');
     next()
   },
   validateRequest(productZodSchema),
